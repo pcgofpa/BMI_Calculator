612155376975aa88fd088cd7ddd1099fa48786aa
pcgofpa <pcgofpa@gmail.com>
2023-06-10 21:36:49 -0500

    Create BMI_Calculator.py
    Push application made to convert height and weight into Body Mass index

---
+++ BMI_Calculator.py
@@ -0,0 +1,11 @@
+#BMI=weight/(height squared)
+
+# Don't change the code below
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+#Write code below this line
+BMI = float(weight)/float(height)**2
+
+#Display BMI output
+print(int(BMI))
